e1248eea8c3c6b877d094b28a090afdad2a50e81
chongkessob <chonks091004@gmail.com>
2025-02-07 18:08:49 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -7,6 +7,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),Infinix-X6532)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+ifneq ($(filter X6532,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+#
