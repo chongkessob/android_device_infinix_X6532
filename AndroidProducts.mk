012f424afff21fe244f39d6bc1824fdb35e48237
chongkessob <chonks091004@gmail.com>
2025-02-07 12:28:53 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X6532.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_X6532.mk
 
-COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X6532-user \
-    omni_Infinix-X6532-userdebug \
-    omni_Infinix-X6532-eng
+COMMON_LUNCH_CHOICES := twrp_X6532-eng
