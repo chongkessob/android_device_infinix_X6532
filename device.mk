6c01290508a83eed7c5d68b56dd34428e7963799
chongkessob <chonks091004@gmail.com>
2025-02-07 12:30:00 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix-X6532
+LOCAL_PATH := device/infinix/X6532
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_TARGET_VNDK_VERSION := 31
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -17,6 +22,19 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6768
@@ -33,3 +51,5 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
