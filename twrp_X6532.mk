51b366dd1f21afd926cb665b5e5c4f98124ef0aa
chongkessob <chonks091004@gmail.com>
2025-02-07 18:44:55 +0800

    Update twrp_X6532.mk

--- twrp_X6532.mk
+++ twrp_X6532.mk
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := X6532
+
+DEVICE_PATH := device/infinix/$(PRODUCT_RELEASE_NAME)
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
@@ -14,15 +20,11 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from Infinix-X6532 device
 $(call inherit-product, device/infinix/X6532/device.mk)
 
-PRODUCT_DEVICE := X6532
-PRODUCT_NAME := twrp_X6532
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X6532
 PRODUCT_MANUFACTURER := infinix
+PRODUCT_MODEL := Infinix X6532
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_x6532_v632-user 12 SP1A.210812.016 722914 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6532-OP/Infinix-X6532:12/SP1A.210812.016/241017V2228:user/release-keys
