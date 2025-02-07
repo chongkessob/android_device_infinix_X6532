8af1eadb6004229e58649e853693dee87d1ff1d5
chongkessob <chonks091004@gmail.com>
2025-02-07 17:28:29 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_system \
     system_ext \
     product
-BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_USES_RECOVERY_AS_BOOT := false
 
 # Architecture
 TARGET_ARCH := arm
